5ec058656133c2c12aeb3f4ddb85b707c4bda5ab
KobaKoba0412 <imagista@outlook.jp>
2020-05-02 12:44:04 +0900

    chat view内をクラス化　＆　 ログインしているユーザーにアクセスを制限　

--- chat/urls.py
+++ chat/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 app_name = 'chat'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:room_name>', views.chat, name='chat_room'),
-    path('room/', views.room, name='room'),
+    path('', login_required(views.index.as_view()), name='index'),
+    path('<str:room_name>', login_required(views.chat.as_view()), name='chat_room'),
+    path('room/', login_required(views.room.as_view()), name='room'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -6,28 +6,36 @@ from django.template import loader
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views import generic
+from django.shortcuts import render
+from django.views.generic.base import View
 
 User = get_user_model()
 
-def index(request):
-    room_list = Room.objects.order_by('-created_at')[:5]
-    template = loader.get_template('chat/index.html')
-    context = {
-        'room_list': room_list,
-    }
-    return HttpResponse(template.render(context, request))
+class index(View):
+    
+    def get(self, request, *args, **kwargs ):
+        room_list = Room.objects.order_by('-created_at')[:5]
+        context = {
+            'room_list': room_list,
+        }
+        return render( request, 'chat/index.html', context )
 
-def chat(request, room_name):
-    messages = Message.objects.filter(room__name=room_name).order_by('-created_at')[:50]
-    room = Room.objects.filter(name=room_name)[0]
-    template = loader.get_template('chat/chat_room.html')
-    context = {
-        'messages': messages,
-        'room': room
-    }
-    return HttpResponse(template.render(context, request))
+class chat(View):
+    
+    def get(self, request, *args, **kwargs ):
+        room_name= kwargs.get('room_name') 
+        messages = Message.objects.filter(room__name=room_name).order_by('-created_at')[:50]
+        room = Room.objects.filter(name=room_name)[0]
+        template = loader.get_template('chat/chat_room.html')
+        context = {
+            'messages': messages,
+            'room': room
+        }
+        return HttpResponse(template.render(context, request))
 
-def room(request):
-    name = request.POST.get("room_name")
-    room = Room.objects.create(name=name)
-    return HttpResponseRedirect(reverse('chat:chat_room', args=[name]))
+class room(View):
+    
+    def post(self, request, *args, **kwargs ):
+        name = request.POST.get("room_name")
+        room = Room.objects.create(name=name)
+        return HttpResponseRedirect(reverse('chat:chat_room', args=[name]))
